6c34a64ee89383c3a0a23202e62990f4359c010e
dev-dong <dho0647@naver.com>
2022-01-17 20:43:42 +0900

    List, Map, HashMap

--- src/javaExD/Solution.java
+++ src/javaExD/Solution.java
@@ -1,32 +1,38 @@
 package javaExD;
 
-import java.util.Arrays;
-
+// List
+// Map
+// HashMap
 class Solution {
     public void solution(String[] id_list, String[] report, int k) {
         int[] cnt = new int[id_list.length];
         int[] result = new int[id_list.length];
-        String[] str = {};
+        String[] tmp = new String[report.length];
+        String[] comTmp = new String[report.length];
+        String[] str = new String[2];
 
         for(int i = 0; i < report.length; i++) {
             str = report[i].split(" ");
+            tmp[i] = str[0];
+            comTmp[i] = str[1];
+        }
 
-            for(int j = 0; j < id_list.length; j++) {
-                if(id_list[j].equals(str[1])) {
+        for(int i = 0; i < report.length; i++) {
+            for(int j = 0; j < id_list.length; j++){
+                if(id_list[j].equals(comTmp[i])) {
                     cnt[j]++;
                 }
             }
+        }
 
-//            for(int j = 0; j < str.length; j++) {
-//                System.out.println("split[" + j + "] : " + str[j]);
-//            }
+        for(int i = 0; i < report.length; i++) {
+            System.out.println(tmp[i]);
         }
 
         for(int i = 0; i < cnt.length; i++) {
             if(cnt[i] >= k) {
 
             }
-            System.out.println(cnt[i]);
         }
 
 //        int[] answer = {};
@@ -36,9 +42,6 @@ class Solution {
     public static void main(String[] args) {
         String[] id_list = {"muzi", "frodo", "apeach", "neo"};
         String[] report = {"muzi frodo", "apeach frodo", "frodo neo","muzi neo", "apeach muzi"};
-        String[] str = report[0].split(" ");
-        System.out.println(str[1]);
-        System.out.println("report 시작 : " + report[0]);
         int k = 2;
         Solution s = new Solution();
         s.solution(id_list, report, k);
